adc0dace76274593eaf214a6e80d255bf3272a56
OkungbowaGodspower <godspowerokungbowa0@gmail.com>
2023-02-23 16:42:08 +0100

    vi 10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangles
+ * * @size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = i; j >= 0; j--)
+			{
+				_putchar(32);
+			}
+			for (j = i; j < size; j++)
+			{
+				_putchar(35);
+			}
+		}
+		_putchar('\n');
+
+	}
+}
