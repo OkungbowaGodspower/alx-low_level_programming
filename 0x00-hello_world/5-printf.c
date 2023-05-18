d9d1bb35d775146bfcfd8c2e1a202c5ec6d8f10f
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-18 18:40:25 +0100

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - starting (entry) point of the program
+  * printf - write the string to standard output
+  *
+  * Return: returns (0) for success
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
