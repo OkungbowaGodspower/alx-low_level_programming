e950b44fde9c04b408586c83abeb63e30e415ff1
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-18 20:05:39 +0100

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
+
   /**
    * main - starting (entry) point of the program
+   * @fputs - writes formatted output to a specified file stream
    *
    * Return: returns (1) for success
    */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 
 	return (1);
 }
