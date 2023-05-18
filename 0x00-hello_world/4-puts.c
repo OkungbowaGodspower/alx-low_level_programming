b6b77073a2b62f51655295678904b1ec135f57db
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-18 18:21:51 +0100

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - starting (entry) point of the program
+  * message - it's a variable that stores the value
+  * @puts - write the string to standard output
+  *
+  * Return: returns (0) for success
+  */
+
+int main(void)
+{
+	char message = "Programming is like building a multilingual puzzle";
+
+	puts(message);
+
+	return (0);
+}
