900245c5a22a8a2051bd81e7f2e6b398443f92c0
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-19 20:49:41 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - starting (entry) point of the program
+  * printf - write the string to standard output
+  *
+  * Return: returns (0) for success
+  */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter < 'z')
+	{
+		putchar(letter++);
+	}
+	putchar('\n');
+	return (0);
+
+}
