5929bf9a789711e36691b8224ad78327fb021857
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-20 08:22:09 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  * main - starting (entry) point of the program
+  * printf - write the string to standard output
+  * Description: Prints all possible combinations of
+  * - single-digit numbers followed by a new line
+  * Return: returns (0) for success
+  */
+int main(void)
+{
+	int num = 0;
+
+	for (; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	if (num < 9)
+	{
+		putchar(',');
+	}
+	if (num < 9)
+	{
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+
+}
