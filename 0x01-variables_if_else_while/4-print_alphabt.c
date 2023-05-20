784f48943f746d19b66b5529905d92c7ac16d0db
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-20 07:19:35 +0100

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,6 +3,8 @@
 /**
   * main - starting (entry) point of the program
   * printf - write the string to standard output
+  * Description: prints lower alphabets from "a - z"
+  * - then skip 'q' and 'e' followed by a new line
   *
   * Return: returns (0) for success
   */
