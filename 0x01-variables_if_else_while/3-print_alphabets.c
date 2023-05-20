1845068ce72fb0858088baf8929f5776f0719f7f
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-20 07:17:35 +0100

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,6 +3,8 @@
 /**
   * main - starting (entry) point of the program
   * printf - write the string to standard output
+  * Desciption: prints lower and upper case letters
+  * from "a - Z" followed by a new line
   *
   * Return: returns (0) for success
   */
