09fdb3128d2636893b2ff0cf953fa7c3f829f105
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-20 07:14:53 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,8 @@
 /**
   * main - starting (entry) point of the program
   * printf - write the string to standard output
-  *
+  * Description: Prints small alphabets from "a - z"
+  * - followed by a new line
   * Return: returns (0) for success
   */
 int main(void)
