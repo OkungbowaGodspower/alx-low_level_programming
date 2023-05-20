91c33a3f01ee0b4e605ea94729c9000d9128d7d1
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-20 07:20:35 +0100

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,9 +3,9 @@
 /**
   * main - starting (entry) point of the program
   * printf - write the string to standard output
-  *
   * Description: print all single digit -
   * - numbers of base 10 starting from 0
+  *
   * Return: returns (0) for success
   */
 int main(void)
