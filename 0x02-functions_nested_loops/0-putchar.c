384264d28ea8cba9f7f27c54821e15d3dde69d17
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-23 16:28:28 +0100

    Prints _putchar to stdout

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - starting (entry) point of the program
+  * printf - write the string to standard output
+  * Description: A program that prints "_putchar";
+  * - followed by a new line
+  * Return: returns (0) for success
+  */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+
+}
