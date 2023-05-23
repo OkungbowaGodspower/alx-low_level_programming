b27dbf4e5bc813edbff60fff839c4a19128a388d
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-23 18:54:39 +0100

    Prints 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * print_alphabet_x10 - print the alphabet in lower case
+  * - from 'a' to 'z' ten times
+  * _putchar: write the string to standard output
+  * - followed by a new line
+  *
+  * Return: returns (0) for success
+  */
+void print_alphabet_x10(void)
+{
+	int a, b;
+	for (a = 0; a < 10; a++)
+	{
+		char letter = 'a';
+
+		for (b  = 0; b < 26; b++)
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+}
