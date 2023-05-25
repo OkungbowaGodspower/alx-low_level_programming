41831dc05538f4c9612b2687992f31455ea2e838
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-25 11:28:23 +0100

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+  * main - starting (entry) point of the program
+  * printf - write the string to standard output
+  *
+  *Return: returns zero (0)
+  */
 
 int main(void)
 {
