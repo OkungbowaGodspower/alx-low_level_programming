4a6019e397bee8a00b5fc96e19092bff8c2a3c3d
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-25 21:04:52 +0100

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints digits from
+ * - from (0 through 9)
+ *
+ * @n: variable to print
+ * Return: return nothing
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n = n + 1)
+		_putchar(n + '0');
+}
