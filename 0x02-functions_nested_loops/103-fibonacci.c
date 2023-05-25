129eb72508078fc9126854d9b97c382035863317
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-05-25 14:14:42 +0100

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 fibonacci numbers
+ * - starting with 1 and 2
+ * Return: returns zer (0)
+ */
+int main(void)
+{
+	int counter = 0;
+	long int x = 1, y = 2, sum = y;
+
+	while (x + y < 4000000)
+	{
+		y = y + x;
+		if (y % 2 == 0)
+		{
+			sum = sum + y;
+			x = y - x;
+			++counter;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
