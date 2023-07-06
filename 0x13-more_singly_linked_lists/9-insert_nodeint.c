f8831e46560fe932fcb1e3e3bd39b818cf8cd64c
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-07-06 14:02:40 +0100

    Refactor: improve readability and add comments to insert_nodeint_at_index function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,46 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - addition of a new node to a linked list
- * at a given position
- * @head - pointer to the head of the list
- * @idx - index of the list where the new node will be added
- * - (indices start at 0)
- * @n - value for the new node to be added
- * Return: pointer to the new node,
- * or NULL - if function fails
+ * insert_nodeint_at_index - Inserts a new node into a linked list
+ *                          at the specified position.
+ * @head: Pointer to the head of the list.
+ * @idx: Index where the new node will be added (indices start at 0).
+ * @n: Value for the new node to be added.
  *
+ * Return: Pointer to the new node, or NULL if the function fails.
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *newNode, *current = *head;
-unsigned int node1;
+	listint_t *newNode, *current = *head;
+	unsigned int node_idx;
 
-newNode = malloc(sizeof(listint_t));
+	/* Create a new node */
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
 
-if (newNode == NULL)
-return (NULL);
+	newNode->n = n;
 
-newNode->n = n;
+	/* Insert at the beginning of the list */
+	if (idx == 0)
+	{
+		newNode->next = current;
+		*head = newNode;
+		return (newNode);
+	}
 
-if (idx == 0)
-{
-newNode->next = current;
-*head = newNode;
-return (newNode);
-}
+	/* Traverse the list to the specified position */
+	for (node_idx = 0; node_idx < (idx - 1); node_idx++)
+	{
+		if (current == NULL || current->next == NULL)
+			return (NULL);
 
-for (node1 = 0; node1 < (idx - 1); node1++)
-{
-if (current == NULL || current->next == NULL)
-return (NULL);
+		current = current->next;
+	}
 
-current = current->next;
-}
+	/* Insert the new node into the list */
+	newNode->next = current->next;
+	current->next = newNode;
 
-newNode->next = current->next;
-current->next = newNode;
-
-return (newNode);
+	return (newNode);
 }
